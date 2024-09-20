388d2ffa895f4ff457ce5c6a1f6d76090784b447
adarshagnihotri0 <113agni@gmail.com>
2024-09-20 19:30:29 +0530

    context api basics

---
+++ contextapi/src/context/UserContext.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+// Create a context for user data
+const UserContext = React.createContext()
+
+// Provider component that provides user data
+export default UserContext;
